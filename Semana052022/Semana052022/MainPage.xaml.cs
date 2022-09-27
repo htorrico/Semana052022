5f5370dd286aa2afdcb3517732bd332b7d5f808e
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2022-09-27 12:02:08 -0500

    Add project files.

---
+++ Semana052022/Semana052022/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana052022
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.dtpFechaNacimiento.Date = DateTime.Today;
+            this.dtpFechaNacimiento.MaximumDate = DateTime.Today;
+        }
+    }
+}
